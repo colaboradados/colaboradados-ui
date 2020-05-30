6bc6803e58a90828b34a3fc50e13dc3de8405214
Ana Tarrisse <tarrisse@work.co>
2020-05-30 19:10:48 -0300

    feat: componente TitleChevron

---
+++ src/components/TitleChevron/TitleChevron.vue
@@ -0,0 +1,29 @@
+<template>
+  <a :href="url">
+    <h2>{{ title }}</h2>
+    <span class="icons">
+      <icon icon="chevron-right" />
+      <icon icon="chevron-right" />
+    </span>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'TitleChevron',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    url: {
+      type: String,
+      default: ''
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+@import './TitleChevron.styles.scss';
+</style>

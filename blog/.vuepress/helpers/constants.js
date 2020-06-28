d63074782e551fdc930b11137e3bf73646da64ce
Ana Tarrisse <tarrisse@work.co>
2020-06-28 14:43:08 -0300

    chore: updated/refactor constants

--- blog/.vuepress/helpers/constants.js
+++ blog/.vuepress/helpers/constants.js
@@ -1,54 +1,59 @@
 export const METADATA = {
+  ANCHORFM: "anchorFM",
   EDITORS: "editors",
   HOSTS: "hosts",
   PARTICIPANTS: "participants",
 };
 
-export const PEOPLE = Object.freeze({
-  "ana-paula-mendes": {
-    name: "Ana Paula Mendes",
-    twitter: "ananoterminal",
+export const PLATFORMS = Object.freeze({
+  ANCHORFM: {
+    url: "https://www.twitter.com",
+    rss: "https://anchor.fm/s/951cc10/podcast/rss",
   },
-  "fernando-masanori": {
-    name: "Fernando Masanori",
-    twitter: "fmasanori",
-  },
-  "fiquem-sabendo": {
-    name: "Fiquem Sabendo",
-    twitter: "_fiquemsabendo",
-  },
-  "joao-ernane": {
-    name: "João Ernane",
-    twitter: "ChofenAdulto",
-  },
-  "judite-cypreste": {
-    name: "Judite Cypreste",
-    twitter: "juditecypreste",
-  },
-  "leticia-silva": {
-    name: "Letícia Silva",
-    twitter: "dii_lua",
-  },
-  "natalia-mazotte": {
-    name: "Natália Mazotte",
-    twitter: "NataliaMazotte",
-  },
-  "raphael-hernandes": {
-    name: "Raphael Hernandes",
-    twitter: "hernandesraph",
-  },
-  "rodrigo-menegat": {
-    name: "Rodrigo Menegat",
-    twitter: "RodrigoMenegat",
-  },
-  turicas: {
-    name: "Turicas",
-    twitter: "turicas",
+  TWITTER: {
+    url: "https://www.twitter.com",
   },
 });
 
-export const PLATFORMS = Object.freeze({
-  TWITTER: {
-    url: "https://www.twitter.com",
+export const PEOPLE = Object.freeze({
+  "ana-paula-mendes": {
+    name: "Ana Paula Mendes",
+    [PLATFORMS.TWITTER]: "ananoterminal",
+  },
+  "fernando-masanori": {
+    name: "Fernando Masanori",
+    [PLATFORMS.TWITTER]: "fmasanori",
+  },
+  "fiquem-sabendo": {
+    name: "Fiquem Sabendo",
+    [PLATFORMS.TWITTER]: "_fiquemsabendo",
+  },
+  "joao-ernane": {
+    name: "João Ernane",
+    [PLATFORMS.TWITTER]: "ChofenAdulto",
+  },
+  "judite-cypreste": {
+    name: "Judite Cypreste",
+    [PLATFORMS.TWITTER]: "juditecypreste",
+  },
+  "leticia-silva": {
+    name: "Letícia Silva",
+    [PLATFORMS.TWITTER]: "dii_lua",
+  },
+  "natalia-mazotte": {
+    name: "Natália Mazotte",
+    [PLATFORMS.TWITTER]: "NataliaMazotte",
+  },
+  "raphael-hernandes": {
+    name: "Raphael Hernandes",
+    [PLATFORMS.TWITTER]: "hernandesraph",
+  },
+  "rodrigo-menegat": {
+    name: "Rodrigo Menegat",
+    [PLATFORMS.TWITTER]: "RodrigoMenegat",
+  },
+  turicas: {
+    name: "Turicas",
+    [PLATFORMS.TWITTER]: "turicas",
   },
 });

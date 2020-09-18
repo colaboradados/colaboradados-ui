3fdecd96352c2eae721aa40dcf7c64febf0846d4
Ana Tarrisse <tarrisse@work.co>
2020-09-17 22:36:21 -0300

    feat: table

--- blog/.vuepress/theme/layouts/Database.vue
+++ blog/.vuepress/theme/layouts/Database.vue
@@ -1,39 +1,57 @@
 <template>
   <Base>
     <h1>{{ frontmatter.title }}</h1>
-    <DB id="database" />
+    <vue-good-table :columns="TABLE_HEADERS" :rows="this.rows" />
   </Base>
 </template>
 
 <script>
-import axios from "axios";
+import { csvParse, csvParseRows, csvFormatBody } from "d3-dsv";
+import { VueGoodTable } from "vue-good-table";
 
-import DB from "@components/DB";
-
-// import api from "@utils/api";
-import { getMetaContent } from "@helpers/get";
+import TABLE_HEADERS from "@constants/tableHeaders";
+import API from "@utils/api";
 
 export default {
   components: {
-    DB,
+    VueGoodTable,
+  },
+  data: function() {
+    return {
+      csvData: "",
+      TABLE_HEADERS,
+    };
   },
   computed: {
+    columns() {
+      return Object.values(this.TABLE_HEADERS).reduce((acc, curr, index) => {
+        if (!curr) return;
+        return { ...acc, [`${curr.field}`]: curr.label };
+      }, {});
+    },
     frontmatter() {
       return this.$page.frontmatter;
+    },
+    rows() {
+      return csvParse(this.csvData, this.formatParser);
     },
   },
   async created() {
     this.isLoading = true;
-    // await axios
-    //   .get(URL_DATABASES)
-    //   .then((response) => {
-    //     this.isLoading = false;
-    //     this.csvData = response.data;
-    //     console.log(response.data);
-    //   })
-    //   .catch((error) => {
-    //     throw Error(error);
-    //   });
+    this.csvData = await API.fetchDatabases();
+    console.log(this.csvData);
+  },
+  methods: {
+    formatParser(item) {
+      const obj = {
+        title: item[this.columns.title],
+        link: item[this.columns.link],
+        scope: item[this.columns.scope],
+        location: "",
+        category: item[this.columns.category],
+      };
+      return obj;
+    },
   },
 };
 </script>

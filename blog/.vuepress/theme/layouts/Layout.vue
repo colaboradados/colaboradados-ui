8eab752e1130e49d83d460725d4872db5b1beb21
Ana Tarrisse <4750042+atarrisse@users.noreply.github.com>
2020-09-22 20:56:06 -0300

    About page

---
+++ blog/.vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,22 @@
+<template>
+  <Base>
+    <h1>{{ frontmatter.title }}</h1>
+    <Content />
+  </Base>
+</template>
+
+<script>
+export default {
+  name: "Layout",
+  computed: {
+    frontmatter() {
+      return this.$page.frontmatter;
+    },
+  },
+  mounted() {
+    console.log(this.frontmatter);
+  },
+};
+</script>
+
+<style></style>
